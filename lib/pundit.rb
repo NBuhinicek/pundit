e13c96e006c39dc5fce7c8f39dfb034550137abf
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-12-17 16:49:42 +0100

    Try me

--- lib/pundit.rb
+++ lib/pundit.rb
@@ -69,7 +69,7 @@ module Pundit
     def authorize(user, record, query, policy_class: nil)
       policy = policy_class ? policy_class.new(user, record) : policy!(user, record)
 
-      raise NotAuthorizedError, query: query, record: record, policy: policy unless policy.public_send(query)
+      raise NotAuthorizedError, query: query, record: record, policy: policy, code: @code unless policy.public_send(query)
 
       record.is_a?(Array) ? record.last : record
     end
